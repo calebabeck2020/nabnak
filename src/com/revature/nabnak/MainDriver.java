51be4435c21d7e244dfd8394ade13a220257f0e9
calebabeck2020 <calebabeck@gmail.com>
2022-07-27 17:49:46 -0400

    6pm 7/27/22 changes

--- src/com/revature/nabnak/MainDriver.java
+++ src/com/revature/nabnak/MainDriver.java
@@ -1,7 +1,10 @@
 package com.revature.nabnak;
 
+import com.revature.nabnak.models.*;
 import java.util.*;
 import java.io.*;
+
+
 
 /* This software is a kanban board for professional developers aimed
     to be the next best virtual board online.
@@ -36,16 +39,20 @@ public class MainDriver {
                 switch (input) {
                     case "1":
                         System.out.println("Login");
-                        login();
                         break;
                     case "2":
                         System.out.println("Register");
+                        register(); // invoke register method
                         break;
                     case "3":
                         System.out.println("Exit");
+                        running = false;
                         break;
                     default:
                         System.out.println("Invalid input, please try again");
+                        System.out.println(welcomeMessages[1]);
+                        System.out.println(welcomeMessages[2]);
+                        System.out.println(welcomeMessages[3]);
                 }
 
             } catch (IOException e) { // catches IOException and assigns it to variable 'e'
@@ -54,25 +61,34 @@ public class MainDriver {
         }
     }
 
-    public static void login() {
+    public static void register() throws IOException {
 
-        try {
-            System.out.println("Please enter your email: \n>");
-            String email = terminalReader.readLine();
+        System.out.println("Please enter your email: ");
+        String email = terminalReader.readLine();
 
-            System.out.println("Please enter your full name: \n>");
-            String fullName = terminalReader.readLine();
+        System.out.println("Please enter your full name: ");
+        String fullName = terminalReader.readLine();
 
-            System.out.println("Please enter months of experience: \n>");
-            String expMonths = terminalReader.readLine();
+        System.out.println("Please enter months of experience: ");
+        String experienceMonths = terminalReader.readLine();
 
-            System.out.println("Please enter your registration date: \n>");
-            String regDate = terminalReader.readLine();
+        System.out.println("Please enter your registration date: ");
+        String registrationDate = terminalReader.readLine();
 
-            System.out.printf("New User registered under:\n %s,%s,%s,%s", email, fullName, expMonths, regDate);
+        System.out.printf("New User registered under:\n %s,%s,%s,%s", email, fullName, experienceMonths, registrationDate);
+
+        File memoryFile = new File("resources/data.txt");
+
+        // try with resources block (EXTENDS AutoCloseable, so file auto-closes
+        try(FileWriter fileWriter = new FileWriter(memoryFile, true);) {
+
+            Member member = new Member(email,fullName,Integer.parseInt(experienceMonths),registrationDate,"password");
+            fileWriter.write(member.writeToFile());
 
         } catch (IOException e) {
+
             e.printStackTrace();
+
         }
     }
 
---
+++ src/com/revature/nabnak/models/Member.java
@@ -0,0 +1,89 @@
+package com.revature.nabnak.models;
+
+// classes are blueprints for a given object
+//
+public class Member {
+    // Section 1: fields/attributes/State/Data
+    private String email;
+    private String fullName;
+    private int experienceMonths;
+    private String registrationDate;
+    private String password;
+
+    // Section 2: Constructors
+    // default no arg constructor
+    public Member() {
+        super(); // super is the class this is inheriting from
+        // the root object of this class is called Object
+    }
+
+    // custom constructor
+    public Member(String email, String fullName, int experienceMonths, String registrationDate, String password) {
+        this.email = email;
+        this.fullName = fullName;
+        this.experienceMonths = experienceMonths;
+        this.registrationDate = registrationDate;
+        this.password = password;
+    }
+
+
+
+    // Section 3: Methods
+
+    // Encapsulation - prevent the manipulation of data outside a class
+    // EMAIL SETTER/GETTER
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email; // inside the scope of the Object
+    }
+
+    // FULL NAME SETTER/GETTER
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    // EXPERIENCE MONTHS SETTER/GETTER
+    public void setExperienceMonths(int experienceMonths) {
+        this.experienceMonths = experienceMonths;
+    }
+
+    public int getExperienceMonths() {
+        return experienceMonths;
+    }
+
+    // REGISTRATION DATE SETTER/GETTER
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public String getRegistrationDate() {
+        return registrationDate;
+    }
+
+    // PASSWORD SETTER/GETTER
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password; // inside the scope of the Object
+    }
+
+
+    public String writeToFile() {
+        return email + "," + fullName + "," + experienceMonths + "," + registrationDate + "," + password + "\n";
+    }
+
+    // Polymorphism
+    @Override
+    public String toString() {
+        return "";
+    }
+}
